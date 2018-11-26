d2c8f2a595261b9bfad90164693a02dfa98fe24b
Vladimir Plaksa <vladimir.plaksa@mysale.com>
2018-11-26 20:22:43 +0300

    add draft async2sync

--- PatternsConsole/Program.cs
+++ PatternsConsole/Program.cs
@@ -1,12 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace PatternsConsole
 {
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int worker;
+    //        int ioCompleteon;
+    //        ThreadPool.GetMaxThreads(out worker, out ioCompleteon);
+
+    //        Console.WriteLine(worker); //32767
+    //        Console.WriteLine(ioCompleteon); //100
+    //        Console.WriteLine("Hello World!");
+    //        Console.Read();
+    //    }
+    //}
+    //class Program
+    //{
+    //    static void DummyCall()
+    //    {
+    //        Thread.Sleep(1000000000);
+    //    }
+
+    //    static void Main(string[] args)
+    //    {
+    //        int count = 0;
+    //        var threadList = new List<Thread>();
+    //        try
+    //        {
+    //            while (true)
+    //            {
+    //                Thread newThread = new Thread(new ThreadStart(DummyCall), 1024);
+    //                newThread.Start();
+    //                threadList.Add(newThread);
+    //                count++;
+    //                if(count%1000 == 0)
+    //                    Console.WriteLine(count);
+    //            }
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Console.WriteLine("Max" +  count);
+    //        }
+    //    }
+    //}
+
+
     class Program
     {
-        static void Main(string[] args)
+        private static async Task<int> Sleep(int ms)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sleeping for " + ms);
+            var task = Task.Run(() => Thread.Sleep(ms));
+            await task;
+            Console.WriteLine("Sleeping for " + ms + "END");
+            return ms;
+        }
+
+        static async void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            Console.WriteLine("Sleeping for " + 2000);
+            var task1 = Task.Run(() => Thread.Sleep(2000));
+
+            Console.WriteLine("Sleeping for " + 2000 + "END");
+
+
+            Console.WriteLine("Sleeping for " + 1000);
+            var task2 = Task.Run(() => Thread.Sleep(1000));
+            await task2;
+            Console.WriteLine("Sleeping for " + 1000 + "END");
+
+            int totalSlept = t1.Result + task2.Result;
+
+            Console.WriteLine("Slept for " + totalSlept + " ms");
+            Console.ReadKey();
         }
     }
+
+
 }
---
+++ Structure/AsyncToSync/AsyncToSync.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Creational.AsyncToSync
+{
+    public class AsyncToSync
+    {
+        public static T RunWithoutContext<T>(Func<Task<T>> factory)
+        {
+            var task = default(Task<T>);
+
+            if (null == SynchronizationContext.Current)
+            {
+                task = factory();
+            }
+            else
+            {
+                task = Task.Run(async () => await factory()
+                    .ConfigureAwait(false));
+            }
+
+            return task.ConfigureAwait(false)
+                .GetAwaiter()
+                .GetResult();
+        }
+    }
+}
+//https://medium.com/bynder-tech/c-why-you-should-use-configureawait-false-in-your-library-code-d7837dce3d7f
+//https://docs.microsoft.com/ru-ru/dotnet/api/system.threading.tasks.task.configureawait?view=netframework-4.7.2
+//https://blogs.msdn.microsoft.com/benwilli/2017/02/09/an-alternative-to-configureawaitfalse-everywhere/
+//https://stackoverflow.com/questions/13489065/best-practice-to-call-configureawait-for-all-server-side-code
+//https://johnthiriet.com/configure-await/
+//https://www.reddit.com/r/dotnet/comments/7nk0uj/when_to_use_configureawaitfalse/
+//https://blog.stephencleary.com/2012/07/dont-block-on-async-code.html
+//https://habr.com/post/162353/
